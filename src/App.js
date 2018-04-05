eec3ec4d9be86293fd6153a71d32ff85ac213585
parc6502 <ibrahim@serafy.co.uk>
2018-04-05 01:25:23 +0100

    Initial layout for top banner set

--- src/App.js
+++ src/App.js
@@ -6,19 +6,25 @@ import {
   Redirect,
   Switch,
 } from 'react-router-dom';
-import { Header, Image, Card } from 'semantic-ui-react';
+import { Header, Image, Card, Grid } from 'semantic-ui-react';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App ui container">
-        <Header as="h1">
-          <Image circular src="https://image.ibb.co/fTtTSm/jrrYC9x.jpg" />
-          {' '}Ibby
-        </Header>
+        <Grid>
+          <Grid.Row verticalAlign='middle'>
+            <Grid.Column width={4}>
+              <Image size='small' circular src="https://image.ibb.co/fTtTSm/jrrYC9x.jpg" />
+            </Grid.Column>
+            <Grid.Column width={12}>
+              <Header as="h1" textAlign='center'>{' '}Ibby</Header>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
 
-        <Card.Group>
+        <Card.Group itemsPerRow="three">
           <Card>
             <Card.Content>
               <Card.Header>Programmer</Card.Header>
@@ -29,6 +35,12 @@ class App extends Component {
             <Card.Content>
               <Card.Header>Foodhall Volunteer</Card.Header>
               <Card.Description>I volunteer at foodhall on a regular basis and am involved with the cafe, the tech branch, as well as a few other activities.</Card.Description>
+            </Card.Content>
+          </Card>
+          <Card>
+            <Card.Content>
+              <Card.Header>Aspiring Artist</Card.Header>
+              <Card.Description>I recently decided to teach myself to teach myself to draw. I plan to become amazing at it, watch this space.</Card.Description>
             </Card.Content>
           </Card>
         </Card.Group>
